d9111ece0ee01a8d811638454d75e0eb2b0d14ab
Brandon Turner <brandontheturner@gmail.com>
2019-03-20 19:33:52 -0500

    adjusted render format and added toggle button

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,20 +1,48 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import State from "../state";
 
 class Reviews extends Component {
   constructor(props) {
-    super(props)
-    render() {
-      return <div>{props.product.reviews.map(reviews, i)}</div>;
+    super(props);
+    console.log(props.product.reviewsArr);
+
+    this.state = {
+      showReviews: false,
+      product: props.product
+    };
+  }
+
+  toggleReviews = () => {
+    this.setState({ showReviews: !this.state.showReviews });
+  };
+
+  render() {
+    if (this.state.showReviews) {
+      return (
+        <div>
+          <button onClick={this.toggleReviews}>Toggle Here</button>
+          {this.state.product.reviewsArr.map((reviews, index) => {
+            return (
+              <div key={index}>
+                {reviews.description},{reviews.rating}
+              </div>
+            );
+          })}
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <button onClick={this.toggleReviews}>Toggle Here</button>
+        </div>
+      );
     }
   }
 }
 
 //take a product from props, how do I code that?
 Reviews.propTypes = {
-  products: PropTypes.object,
-  categories: PropTypes.object
+  products: PropTypes.object
 };
 
 export default Reviews;
